587ccdeb480b3e62ef93401ae6bb9297f9ec7d24
fran <francisco.chiarino@gmail.com>
2020-03-18 13:57:57 +0100

    Will try to fix image

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import '../style/App.css'
 import CategoriesContainer from './CategoriesContainer';
+import CardsContainer from './CardsContainer';
 
 function App() {
     return (
         <div className="App">
             <SearchBar />
             <CategoriesContainer />
+            <CardsContainer />
         </div>
     );
 }
