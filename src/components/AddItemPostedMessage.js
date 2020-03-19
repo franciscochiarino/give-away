a4f17ee7e0aeba68cb03436c6c3e7c31bb3ee9e3
Francisco Chiarino <francisco.chiarino@gmail.com>
2020-03-19 19:04:32 +0100

    Finished styling thanks message

--- src/components/AddItemPostedMessage.js
+++ src/components/AddItemPostedMessage.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function AddItemPostedMessage() {
     return (
-        <div>
-            <h3>Your item has been successfully posted!</h3>
+        <div className="addItemPostedMessage">
+            <h2>Thank you!</h2>
+            <img src={require("../assets/enjoy.png")} alt="an adult and a child with their hands up" width="80" />
+            <h2>Your item has been successfully posted.</h2>
         </div>
     )
 }
