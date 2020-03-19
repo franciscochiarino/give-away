0a6f9e28fbb72cb0356520a7c67356dce5a3190f
fran <francisco.chiarino@gmail.com>
2020-03-19 17:21:29 +0100

    Added AddItemPostedMessage.js component

---
+++ src/components/AddItemPostedMessage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddItemPostedMessage() {
+    return (
+        <div>
+            <h3>Your item has been successfully posted!</h3>
+        </div>
+    )
+}
