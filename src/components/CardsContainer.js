7d80ade87c09dd6bc01a338191f71491fd2f1375
fran <francisco.chiarino@gmail.com>
2020-03-20 20:05:41 +0100

    Filter cards by category is now possible

--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -1,24 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {connect} from 'react-redux'
 import '../style/CardsContainer.css'
 import Card from './Card'
 
 function CardsContainer(props) {
 
-    // Filter searched category 
-    let filteredItems = [...props.state.items];
-
-    if (props.state.searchCategory) {
-        filteredItems.filter(item => item.category === props.state.searchCategory);
-    } 
+    // Filter cards by category
+    const filterCategory = props.state.items.filter(item => {
+        return item.category.includes(props.state.searchCategory);
+    })
 
     // Get items from Reducer
-    const displayItems = filteredItems.map(item => {
+    const displayItems = filterCategory.map(item => {
         return (
             <Card key={item.id} id={item.id} title={item.title} location={item.location} img={item.imgSrc} description={item.description}/> 
         )
     })
-
+    
     return (
         <div className="cardsContainer">
             {displayItems}
